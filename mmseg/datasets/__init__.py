c614e2af4d6e156b698016b94407b932bae5026a
“ruijieren98” <ruijieren98@gmail.com>
2022-02-04 23:56:05 +0000

    add densepass dataset

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -3,6 +3,7 @@ from .ade import ADE20KDataset
 from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .chase_db1 import ChaseDB1Dataset
 from .cityscapes import CityscapesDataset
+from .densepass import DensePASSDataset
 from .coco_stuff import COCOStuffDataset
 from .custom import CustomDataset
 from .dark_zurich import DarkZurichDataset
@@ -20,7 +21,7 @@ from .voc import PascalVOCDataset
 
 __all__ = [
     'CustomDataset', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
-    'DATASETS', 'build_dataset', 'PIPELINES', 'CityscapesDataset',
+    'DATASETS', 'build_dataset', 'PIPELINES', 'CityscapesDataset', 'DensePASSDataset',
     'PascalVOCDataset', 'ADE20KDataset', 'PascalContextDataset',
     'PascalContextDataset59', 'ChaseDB1Dataset', 'DRIVEDataset', 'HRFDataset',
     'STAREDataset', 'DarkZurichDataset', 'NightDrivingDataset',
